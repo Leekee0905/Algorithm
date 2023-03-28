3152e20bc50a3d4556f8f3639d5ae671aeb73f24
GiSeong Lee <59133563+Leekee0905@users.noreply.github.com>
2023-03-28 16:23:38 +0900

    [level 1] Title: 숫자 짝꿍, Time: 0.19 ms, Memory: 33.5 MB -BaekjoonHub

---
+++ 프로그래머스/lv1/131128. 숫자 짝꿍/숫자 짝꿍.js
@@ -0,0 +1,14 @@
+function solution(X, Y) {
+    let friend = '';
+    X = X.split('');
+    Y = Y.split('');
+    for(let i=0; i<10; i++){
+        let tempX = X.filter(a=>String(i)===a).length;
+        let tempY = Y.filter(a=>String(i)===a).length;
+        friend +=String(i).repeat(Math.min(tempX,tempY))
+    }
+if(friend === "") return "-1"
+    if(Number(friend)===0) return "0"
+    
+    return friend.split("").sort((a,b)=>b-a).join("")
+}
