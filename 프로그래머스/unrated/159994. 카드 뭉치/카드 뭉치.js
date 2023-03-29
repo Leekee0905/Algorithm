b7538ff1bb7679bfb82544d5ae9adaf875dea217
GiSeong Lee <59133563+Leekee0905@users.noreply.github.com>
2023-03-29 17:03:47 +0900

    [unrated] Title: 카드 뭉치, Time: 0.05 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/unrated/159994. 카드 뭉치/카드 뭉치.js
+++ 프로그래머스/unrated/159994. 카드 뭉치/카드 뭉치.js
@@ -1,13 +1,12 @@
 function solution(cards1, cards2, goal) {
     let stack = [];
-    let tempCard1 = cards1
-    let tempCard2 = cards2
+
     for(let i=0; i<goal.length;i++){
-        if(goal[i] === tempCard1[0]){
-            stack.push(tempCard1.shift())
+        if(goal[i] === cards1[0]){
+            stack.push(cards1.shift())
         }
-        else if(goal[i] === tempCard2[0]){
-            stack.push(tempCard2.shift())
+        else if(goal[i] === cards2[0]){
+            stack.push(cards2.shift())
         }
         else return "No"
 
@@ -15,5 +14,4 @@ function solution(cards1, cards2, goal) {
     
     if(stack.toString() == goal)
         return "Yes"
-
 }
