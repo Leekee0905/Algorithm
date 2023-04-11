a77147180f4603feb8fdc314ad43ba301119c4a3
GiSeong Lee <59133563+Leekee0905@users.noreply.github.com>
2023-04-11 16:36:10 +0900

    [unrated] Title: 달리기 경주, Time: 0.17 ms, Memory: 33.5 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/178871. 달리기 경주/달리기 경주.js
@@ -0,0 +1,19 @@
+function solution(players, callings) {
+    var answer = [];
+    let map1 = {};
+    players.map((e,idx)=>map1[e] = idx)
+    for(let i=0; i<callings.length; i++){
+        const overtaker = callings[i]
+        const overtakerIdx = map1[overtaker]
+        const caught = players[overtakerIdx-1]
+        
+        players[overtakerIdx - 1] = overtaker
+        players[overtakerIdx] = caught
+        
+        map1[overtaker] = overtakerIdx - 1
+        map1[caught] = overtakerIdx
+        
+    }
+    
+    return players;
+}
