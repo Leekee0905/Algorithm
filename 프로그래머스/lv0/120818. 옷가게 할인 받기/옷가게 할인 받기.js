bf21df82140b9385817c18eb51e70afbeb670041
GiSung Lee <cj8928@gmail.com>
2023-04-18 18:25:30 +0900

    [level 0] Title: 옷가게 할인 받기, Time: 0.03 ms, Memory: 33.4 MB -BaekjoonHub

---
+++ 프로그래머스/lv0/120818. 옷가게 할인 받기/옷가게 할인 받기.js
@@ -0,0 +1,9 @@
+function solution(price) {
+    if(price >= 100000 && price < 300000){
+        return Math.floor(0.95 * price)
+    }else if(price >= 300000 && price < 500000){
+        return Math.floor(0.9 * price)
+    }else if(price >= 500000){
+        return Math.floor(0.8 * price)
+    }else return Math.floor(price)
+}
