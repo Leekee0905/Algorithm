d8a32579e8d81d3b63ad3eb1519dbde971ac7100
GiSung Lee <cj8928@gmail.com>
2023-05-09 15:39:59 +0900

    [level 2] Title: 행렬의 곱셈, Time: 5.17 ms, Memory: 37.9 MB -BaekjoonHub

---
+++ 프로그래머스/lv2/12949. 행렬의 곱셈/행렬의 곱셈.js
@@ -0,0 +1,15 @@
+function solution(arr1, arr2) {
+    var answer = [];
+    for(let i=0; i<arr1.length;i++){
+        let result = []
+        for(let j=0; j<arr2[0].length; j++){
+            let ele = 0;
+            for(let k=0; k<arr2.length; k++){
+                ele += arr1[i][k] * arr2[k][j];
+            }
+            result.push(ele)
+        }
+        answer.push(result)
+    }
+    return answer;
+}
