330275ec0c9ecb7fd288fe79f3c3eff6d3cc43b1
GiSung Lee <cj8928@gmail.com>
2023-05-23 15:48:20 +0900

    [level 0] Title: 평행, Time: 0.06 ms, Memory: 33.5 MB -BaekjoonHub

---
+++ 프로그래머스/lv0/120875. 평행/평행.js
@@ -0,0 +1,16 @@
+function solution(dots) {
+    var answer = 0;
+    const calculation = (a,b,c,d) => {
+        let abDiff, cdDiff=0;
+        abDiff = (b[1]-a[1])/(b[0]-a[0])
+        cdDiff = (d[1]-c[1])/(d[0]-c[0])
+        if(abDiff===cdDiff){
+            answer++
+        }
+    }
+    calculation(dots[0],dots[1],dots[2],dots[3])
+    calculation(dots[0],dots[2],dots[1],dots[3])
+    calculation(dots[0],dots[3],dots[1],dots[2])
+    
+    return answer > 0 ? 1 : 0;
+}
