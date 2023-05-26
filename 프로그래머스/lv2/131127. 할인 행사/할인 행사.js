3c6414ea471be418cc43cc77a9b6957de72780c9
GiSung Lee <cj8928@gmail.com>
2023-05-26 16:46:58 +0900

    [level 2] Title: 할인 행사, Time: 0.09 ms, Memory: 33.6 MB -BaekjoonHub

---
+++ 프로그래머스/lv2/131127. 할인 행사/할인 행사.js
@@ -0,0 +1,23 @@
+function solution(want, number, discount) {
+    let answer = 0;
+    if(!discount.includes(want[0]) && want.length === 1){
+        return 0
+    }
+    
+    for(let i=0; i<discount.length; i++){
+        let discountProduct = discount.slice(i,i+10)
+
+        for(let j=0; j< want.length; j++){
+            let wantedCount = discountProduct.filter(e=>e===want[j]).length
+
+            if(wantedCount >= number[j]){
+                if(j===want.length-1){
+                    answer++
+                }
+            }else
+                break;
+            
+        }
+    }
+    return answer
+}
