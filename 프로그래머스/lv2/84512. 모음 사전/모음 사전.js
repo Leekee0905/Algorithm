4bf735a65668c9f299a1956d2fa5978ae8475c92
GiSung Lee <cj8928@gmail.com>
2023-06-18 22:22:12 +0900

    [level 2] Title: 모음 사전, Time: 4.68 ms, Memory: 38.1 MB -BaekjoonHub

---
+++ 프로그래머스/lv2/84512. 모음 사전/모음 사전.js
@@ -0,0 +1,18 @@
+function solution(word) {
+    const alphabet = [...'AEIOU'];
+    const result = {};
+    let idx = 0;
+    
+    const dfs = (words,length)=>{
+        if(length > 5)
+            return ;
+        result[words] = idx++
+        alphabet.forEach(e=>{
+            dfs(words+e,length+1)
+        })
+        
+    }
+    
+    dfs("",0)
+    return result[word];
+}
