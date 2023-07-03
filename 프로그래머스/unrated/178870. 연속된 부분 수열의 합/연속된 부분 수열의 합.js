63dfe513c3fb2bab2ea8beef9834dc9067bf1683
GiSung Lee <cj8928@gmail.com>
2023-07-03 16:28:29 +0900

    [unrated] Title: 연속된 부분 수열의 합, Time: 0.06 ms, Memory: 33.5 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/178870. 연속된 부분 수열의 합/연속된 부분 수열의 합.js
@@ -0,0 +1,28 @@
+function solution(sequence, k) {
+    var answer = [];
+    let left = 0;
+    let right = 0;
+    let sum = sequence[0]
+    
+    while(right < sequence.length){
+        if(sum < k){
+            right++;
+            sum += sequence[right]
+        }else if(sum > k){
+            sum -= sequence[left]
+            left++
+        }else{
+            answer.push([left,right])
+            right++;
+            sum += sequence[right]
+        }
+    }
+    
+    const condition = (a,b) => {
+        const lenDiff = Math.abs(a[0]-a[1]) - Math.abs(b[0]-b[1])
+        if(lenDiff!==0)
+            return lenDiff
+        return a[0] - b[0]
+    }
+    return answer.sort(condition)[0];
+}
