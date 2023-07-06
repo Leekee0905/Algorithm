facfff049ac5dd43785620a5d5a38043ed5a86d7
GiSung Lee <cj8928@gmail.com>
2023-07-06 15:35:35 +0900

    [unrated] Title: l로 만들기, Time: 13.30 ms, Memory: 39.6 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/181834. l로 만들기/l로 만들기.js
@@ -0,0 +1,10 @@
+function solution(myString) {
+    var answer = '';
+    for(let i=0; i<myString.length; i++){
+        if(myString[i].charCodeAt() < "l".charCodeAt())
+            answer+='l'
+        else
+            answer+=myString[i]
+    }
+    return answer;
+}
