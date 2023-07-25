a0e27c33f4892abb65eb17eebe098d33107b6a42
GiSung Lee <cj8928@gmail.com>
2023-07-25 16:18:30 +0900

    [level 2] Title: 다리를 지나는 트럭, Time: 0.09 ms, Memory: 33.6 MB -BaekjoonHub

---
+++ 프로그래머스/lv2/42583. 다리를 지나는 트럭/다리를 지나는 트럭.js
@@ -0,0 +1,22 @@
+function solution(bridge_length, weight, truck_weights) {
+    let passing = []
+    let sum = 0;
+    let sec = 0;
+    
+    while(truck_weights.length || passing.length){
+        if(weight >= sum + truck_weights[0] && passing.length <= bridge_length){
+            const truck = truck_weights.shift();
+            sum+=truck;
+            passing.push([truck, sec+bridge_length])
+            sec++
+        }else { 
+            const [truck, passedSec] = passing.shift();
+            if (sec < passedSec) {
+              sec = passedSec; 
+            }
+            sum -= truck;
+          }
+    }
+    
+    return sec+1;
+}
