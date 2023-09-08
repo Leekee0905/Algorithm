99bf60533e98752fd901263d32e329afa4a69695
GiSung Lee <cj8928@gmail.com>
2023-09-08 11:41:30 +0900

    [unrated] Title: 0 떼기, Time: 0.02 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/unrated/181847. 0 떼기/0 떼기.js
+++ 프로그래머스/unrated/181847. 0 떼기/0 떼기.js
@@ -1,17 +1,3 @@
 function solution(n_str) {
-    let answer = []
-    let arr = n_str.split('')
-    for(let i=0; i<arr.length; i++){
-        if(arr[i] === '0'){
-            if(answer.length > 0){
-                answer.push(arr[i])
-            }else{
-                continue;
-            }
-        }else{
-            answer.push(arr[i])
-            
-        }
-    }
-    return answer.join('');
+    return Number(n_str).toString();
 }
