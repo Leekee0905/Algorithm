e7dcc2bd3fa29b44a8b83f634240a3961c8fba43
GiSung Lee <cj8928@gmail.com>
2024-01-09 23:31:29 +0900

    [Silver I] Title: 스타트링크, Time: 368 ms, Memory: 21784 KB -BaekjoonHub

--- 백준/Silver/5014. 스타트링크/스타트링크.js
+++ 백준/Silver/5014. 스타트링크/스타트링크.js
@@ -4,20 +4,25 @@ const [F, S, G, U, D] = require('fs')
   .trim()
   .split(' ')
   .map(Number);
-const bfs = (top, start, end, up, down) => {
-  const visited = Array(1000001).fill(0);
-  const queue = [[start, 0]];
+const bfs = (stair) => {
+  const visited = new Array(1000001).fill(0);
+  const queue = [[stair, 0]];
   while (queue.length) {
     const [floor, count] = queue.shift();
-    if (visited[floor]) continue;
-    if (floor === end) return count;
-    for (const stairs of [floor + up, floor - down]) {
-      if (stairs >= 1 && stairs <= top && !visited[floor])
-        queue.push([stairs, count + 1]);
+    if (visited[floor]) {
+      continue;
+    }
+    if (floor === G) {
+      return count;
+    }
+    for (const next of [floor + U, floor - D]) {
+      if (next >= 1 && next <= F && !visited[floor]) {
+        queue.push([next, count + 1]);
+      }
     }
     visited[floor] = 1;
   }
   return 'use the stairs';
 };
 
-console.log(bfs(F, S, G, U, D));
+console.log(bfs(S));
