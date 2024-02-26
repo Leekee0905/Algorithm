2d4ca7ebb8b2709cddfab11183bb0cde65c84515
GiSung Lee <cj8928@gmail.com>
2024-02-26 15:53:41 +0900

    [level 2] Title: 전력망을 둘로 나누기, Time: 3.21 ms, Memory: 36.6 MB -BaekjoonHub

---
+++ 프로그래머스/2/86971. 전력망을 둘로 나누기/전력망을 둘로 나누기.js
@@ -0,0 +1,33 @@
+function solution(n, wires) {
+    var answer = Number.MAX_SAFE_INTEGER;
+    const tree = Array.from(Array(n+1),()=>[])
+    wires.forEach(e=>{
+        let [a,b] = e
+        tree[a].push(b)
+        tree[b].push(a)
+    })
+    
+    const bfs = (root, v) => {
+        const visited = Array.from(Array(n+1)).fill(0)
+        const queue = [root]
+        let count = 0;
+        visited[root] = 1;
+        while(queue.length){
+            const index = queue.shift();
+            tree[index].forEach(e=>{
+                if(e !== v && !visited[e]){
+                    visited[e] = 1;
+                    queue.push(e)
+                }
+            })
+            count++;
+        }
+        return count;
+    }
+    
+    wires.forEach(ele=>{
+        let [a,b] = ele;
+        answer = Math.min(answer, Math.abs(bfs(a,b)-bfs(b,a)))
+    })
+    return answer;
+}
