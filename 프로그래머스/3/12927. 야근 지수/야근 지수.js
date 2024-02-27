af9cc490798f57fa6b415cb6ad5e49f233171135
GiSung Lee <cj8928@gmail.com>
2024-02-27 14:59:59 +0900

    [level 3] Title: 야근 지수, Time: 22.34 ms, Memory: 36.7 MB -BaekjoonHub

---
+++ 프로그래머스/3/12927. 야근 지수/야근 지수.js
@@ -0,0 +1,18 @@
+function solution(n, works) {
+    if(n >= works.reduce((acc,cur)=>acc+cur)) return 0;
+    works.sort((a,b)=>b-a);
+    while(n > 0){
+        const max = works[0]
+        for(let i=0; i<works.length; i++){
+            
+            if(works[i] >= max){
+                n--;
+                works[i]--;
+            }
+            
+            if(n <= 0) break;
+        }
+        
+    }
+    return works.reduce((acc,cur)=>acc+cur**2,0);
+}
