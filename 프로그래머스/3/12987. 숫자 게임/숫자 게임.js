162cb24c28f742c01c402b8dd73c607d2952a6c4
GiSung Lee <cj8928@gmail.com>
2024-02-29 16:26:00 +0900

    [level 3] Title: 숫자 게임, Time: 75.06 ms, Memory: 45 MB -BaekjoonHub

---
+++ 프로그래머스/3/12987. 숫자 게임/숫자 게임.js
@@ -0,0 +1,14 @@
+function solution(A, B) {
+    let answer = 0;
+    A.sort((a,b)=>b-a)
+    B.sort((a,b)=>a-b)
+    for(let i=0; i<A.length; i++){
+        const max = B.at(-1);
+        if(A[i] < max){
+            answer++;
+            B.pop()
+        }
+        
+    }
+    return answer;
+}
