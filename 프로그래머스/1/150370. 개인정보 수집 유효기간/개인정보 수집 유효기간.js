99a3720d7198e671c06b9c1fb3ced0855b4c6d2c
GiSung Lee <cj8928@gmail.com>
2024-04-12 15:30:29 +0900

    [level 1] Title: 개인정보 수집 유효기간, Time: 0.81 ms, Memory: 34.2 MB -BaekjoonHub

---
+++ 프로그래머스/1/150370. 개인정보 수집 유효기간/개인정보 수집 유효기간.js
@@ -0,0 +1,43 @@
+function solution(today, terms, privacies) {
+    const obj = {};
+
+    terms.forEach((i) => {
+        obj[i[0]] = parseInt(i.split(' ')[1]);
+    });
+
+    const date = privacies.map(i => {
+        let a = i.split(' ');
+        return a[0].split('.').map(i => parseInt(i));
+    });
+    
+    const reserve = privacies.map((i) => {
+        let a = i.split(' ');
+        return a[1];
+    });
+
+    for(let i = 0 ; i < reserve.length ; i++){
+      date[i][1] = date[i][1] + obj[reserve[i]];
+
+        if(date[i][1] > 12){
+            date[i][1] = date[i][1] - 12;
+            date[i][0]++;
+        }
+
+    }
+
+    today = today.split('.').map(i => parseInt(i))
+
+    let result = [];
+
+    date.forEach((i,idx) => {
+
+        const a = new Date(...today);
+        const b = new Date(...i);
+
+        if( a >= b ){
+            result.push(idx+1);
+        }
+    });
+
+    return result;
+}
