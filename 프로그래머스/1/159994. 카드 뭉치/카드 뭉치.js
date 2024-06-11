d2cc843b525a6051a4573af48159882c981c5736
GiSung Lee <cj8928@gmail.com>
2024-06-11 14:13:34 +0900

    [level 1] Title: 카드 뭉치, Time: 0.05 ms, Memory: 33.5 MB -BaekjoonHub

---
+++ 프로그래머스/1/159994. 카드 뭉치/카드 뭉치.js
@@ -0,0 +1,17 @@
+function solution(cards1, cards2, goal) {
+    let stack = [];
+
+    for(let i=0; i<goal.length;i++){
+        if(goal[i] === cards1[0]){
+            stack.push(cards1.shift())
+        }
+        else if(goal[i] === cards2[0]){
+            stack.push(cards2.shift())
+        }
+        else return "No"
+
+    }
+    
+    if(stack.toString() == goal)
+        return "Yes"
+}
