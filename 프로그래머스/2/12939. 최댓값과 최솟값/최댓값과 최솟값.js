4d630cfac94ea0a53813eadad384d2304b499dbf
GiSung Lee <cj8928@gmail.com>
2024-06-13 17:38:23 +0900

    [level 2] Title: 최댓값과 최솟값, Time: 0.20 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/2/12939. 최댓값과 최솟값/최댓값과 최솟값.js
+++ 프로그래머스/2/12939. 최댓값과 최솟값/최댓값과 최솟값.js
@@ -1,11 +1,7 @@
 function solution(s) {
-    var answer = '';
-    s.split(' ').sort((a,b)=>a-b).map((e,idx)=>{
-        if(idx === 0 || s.split(' ').length-1 === idx ){
-            answer+=e
-            answer+=' '
-        }
+    return s.split(' ').sort((a,b)=>a-b).filter((e,idx)=>{
+        if(idx===0||idx===s.split(' ').length-1)
+            return e
         
-    })
-    return answer.trim();
+    }).join(" ");
 }
