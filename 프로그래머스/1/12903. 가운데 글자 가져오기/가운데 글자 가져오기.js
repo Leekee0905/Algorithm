9e04253c9c8b7d9f8df75dd281bc25c6a3152802
GiSung Lee <cj8928@gmail.com>
2024-08-01 20:36:47 +0900

    [level 1] Title: 가운데 글자 가져오기, Time: 0.05 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/1/12903. 가운데 글자 가져오기/가운데 글자 가져오기.js
+++ 프로그래머스/1/12903. 가운데 글자 가져오기/가운데 글자 가져오기.js
@@ -1,9 +1,3 @@
 function solution(s) {
-    var answer = '';
-    if(s.length % 2 === 0){
-        answer += s[s.length/2-1] + s[s.length/2]
-    }else{
-        answer += s[s.length/2-0.5]
-    }
-    return answer
+    return s.length % 2 === 0 ? s[s.length/2-1] + s[s.length/2] : s[s.length/2-0.5]
 }
