899014e4db12077b9958aa41424dab10af5feed7
GiSung Lee <cj8928@gmail.com>
2024-09-06 09:47:26 +0900

    [level 2] Title: 유사 칸토어 비트열, Time: 674.33 ms, Memory: 36.1 MB -BaekjoonHub

---
+++ 프로그래머스/2/148652. 유사 칸토어 비트열/유사 칸토어 비트열.js
@@ -0,0 +1,17 @@
+function check(num){
+  if (num < 5 && num != 2) return true;
+  if ((num - 2) % 5 === 0) return false;
+
+  return check(Math.floor(num / 5));
+}
+
+
+function solution(n, l, r) {
+    var answer = 0;
+    for(var i = l-1; i<r;i++){
+      if(check(i)){
+        answer++;
+      }
+    }
+    return answer;
+}
