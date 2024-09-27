23bdf1bdb2fa61de019d7dbbc869086756204f0b
GiSung Lee <cj8928@gmail.com>
2024-09-27 09:46:14 +0900

    [level 3] Title: 연속 펄스 부분 수열의 합, Time: 10.78 ms, Memory: 56.8 MB -BaekjoonHub

--- 프로그래머스/3/161988. 연속 펄스 부분 수열의 합/연속 펄스 부분 수열의 합.js
+++ 프로그래머스/3/161988. 연속 펄스 부분 수열의 합/연속 펄스 부분 수열의 합.js
@@ -4,6 +4,7 @@ function solution(sequence) {
 
     for (let i = 0; i < sequence.length; i++) {
         acc = i % 2 == 1 ? acc - sequence[i] : acc + sequence[i];
+        // 누적합 최대값에서 최소값을 빼면 항상 최대, 5번째까지 더한 값에서 2번째까지 더한값을 빼면 3~5번째 부분수열
         M = Math.max(M, acc);
         m = Math.min(m, acc);
     }
