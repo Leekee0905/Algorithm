085a5458950acfdd2e9e6c04617b6e949dcafa20
GiSung Lee <cj8928@gmail.com>
2024-09-27 09:38:45 +0900

    [level 3] Title: 연속 펄스 부분 수열의 합, Time: 10.22 ms, Memory: 56.8 MB -BaekjoonHub

--- 프로그래머스/3/161988. 연속 펄스 부분 수열의 합/연속 펄스 부분 수열의 합.js
+++ 프로그래머스/3/161988. 연속 펄스 부분 수열의 합/연속 펄스 부분 수열의 합.js
@@ -1,26 +1,11 @@
 function solution(sequence) {
-    let answer = -Infinity
-    const len = sequence.length
-    
-    const pulsed = Array.from({length: len},()=>Array.from({length:2},()=>[0,0]))
-    const acc = Array.from({length: len},()=>Array.from({length:2},()=>[0,0]))
-    
-    pulsed[0][0] = sequence[0]
-    pulsed[0][1] = -sequence[0]
-    
-    acc[0][0] = pulsed[0][0]
-    acc[0][1] = pulsed[0][1]
-    answer = Math.max(answer, acc[0][0], acc[0][1])
-    for(let i=1; i<len;i++){
-        const sign = i%2===0 ? 1 : -1
-        
-        pulsed[i][0] = sign * sequence[i]
-        pulsed[i][1] = -1 * sign * sequence[i]
-        
-        acc[i][0] = Math.max(acc[i-1][0] + pulsed[i][0], pulsed[i][0]);
-        acc[i][1] = Math.max(acc[i-1][1] + pulsed[i][1], pulsed[i][1]);
+    let m = 0, M = 0;
+    let acc = m;
 
-        answer = Math.max(answer, acc[i][0], acc[i][1]);
+    for (let i = 0; i < sequence.length; i++) {
+        acc = i % 2 == 1 ? acc - sequence[i] : acc + sequence[i];
+        M = Math.max(M, acc);
+        m = Math.min(m, acc);
     }
-    return answer;
+    return M == m ? M : M - m;
 }
