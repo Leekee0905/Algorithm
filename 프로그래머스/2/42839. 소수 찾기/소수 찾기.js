0c12876a18b0565cd7237f61200ce41a8c9bf6ca
GiSung Lee <cj8928@gmail.com>
2025-02-27 15:38:47 +0900

    [level 2] Title: 소수 찾기, Time: 6.77 ms, Memory: 37.4 MB -BaekjoonHub

--- 프로그래머스/2/42839. 소수 찾기/소수 찾기.js
+++ 프로그래머스/2/42839. 소수 찾기/소수 찾기.js
@@ -1,13 +1,10 @@
 function isPrime(n) {
-  if (n < 2) return false;
-  if (n === 2) return true;
-  if (n % 2 === 0) return false;
-  
-  for (let i = 3; i * i <= n; i += 2) {
-    if (n % i === 0) return false;
-  }
-  
-  return true;
+    if (n < 2) return false;
+    if (n === 2) return true;
+    for (var i = 2; i <= Math.sqrt(n); i++) {
+        if (n%i===0) return false;
+    }
+    return true;
 }
 
 function generateNumbers(str, used, current, primeSet) {
@@ -26,7 +23,7 @@ function generateNumbers(str, used, current, primeSet) {
   }
 }
 
-// 주어진 문자열에서 소수 개수 찾기
+
 function solution(str) {
   const primeSet = new Set();
   generateNumbers(str.split(""), Array(str.length).fill(false), [], primeSet);
