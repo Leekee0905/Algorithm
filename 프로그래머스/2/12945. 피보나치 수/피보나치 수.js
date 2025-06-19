8833c292ae8af88df07fd34441fcebe1f01841c9
GiSung Lee <cj8928@gmail.com>
2025-06-19 14:39:52 +0900

    [level 2] Title: 피보나치 수, Time: 6.54 ms, Memory: 38.8 MB -BaekjoonHub

--- 프로그래머스/2/12945. 피보나치 수/피보나치 수.js
+++ 프로그래머스/2/12945. 피보나치 수/피보나치 수.js
@@ -1,14 +1,8 @@
 function solution(n) {
     let result = []
     for(let i=0; i<=n; i++){
-        if(i===0 || i===1){
-            result.push(i)
-        }
-        else{
-            let sum = result[i-2] + result[i-1]
-            result.push(sum%1234567)
-        }
+        if(i<2) result.push(i)
+        else result.push((result[i-2]+result[i-1]) %  1234567)
     }
-
-    return result[n];
+    return result[n]
 }
